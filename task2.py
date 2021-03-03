1c47469d5b5f4bb1ffbdb24824fdcfddc6f04fc6
Ziii-l <ziweil101@deltalearns.ca>
2021-03-03 10:36:18 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,14 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+
+def largest():
+    # input:
+    #       a list
+    #return value: the largest value in the list
+    return  list[-1]
+#this should be return a value of 13
+x = ([3,1,4,7,13,9])
+
+#this should be return a value of 12.3
+y = ([5,1,12.3]) 
