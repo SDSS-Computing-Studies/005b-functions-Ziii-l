87e68158470684f03efbf14015d3cc240af8e701
Ziii-l <ziweil101@deltalearns.ca>
2021-03-03 10:55:02 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -6,3 +6,12 @@ The return value is the sum of all the numbers in the list
 added together
 (2 points)
 """
+
+def perimeter(number):
+    #input: a list of number
+    #return value: the sum of all number in the list
+    return sum(number)
+#this should return a value of 13
+x = ( [5,2,6])
+#this should return a value of 28.5
+y = ( [9,8,6,5.5] ) 
