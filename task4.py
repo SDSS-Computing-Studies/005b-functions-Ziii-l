bff9a151b55edf4e6fcb7e3dc03806b42e889f81
Ziii-l <ziweil101@deltalearns.ca>
2021-03-03 11:01:57 -0800

    Update task4.py

--- task4.py
+++ task4.py
@@ -7,3 +7,14 @@ Return False if the number is not an integer
 (2 points)
 """
 
+def isInteger( number):
+    # input: a float number
+    # return: True if the number is an integer
+    # return: Flase,if the number is not a integer
+    return number.isInteger()
+# This should return Flase
+x = ( 9.5 )
+# This should be return True
+y = ( -2 )
+
+
