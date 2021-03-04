b755b7f84762164c54a365d9eeaad42aabb3b69e
Ziii-l <ziweil101@deltalearns.ca>
2021-03-03 22:33:14 -0800

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -13,7 +13,7 @@ output
 """
 def factors(number):
     list= []
-    print("The factors of",number,"are: ")
+    print("factors (",number,") == ")
     for whole_number in range(1,number + 1):
         if number % whole_number == 0:
             list.append(-whole_number)
